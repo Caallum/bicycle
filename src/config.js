359041308ad3a275d77c2bc6675203abcfb45bb5
Tur-ph <simplehaxdev@gmail.com>
2021-11-13 20:48:06 +0000

    commit on 'master'

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    "token": "ODU5ODUyNzk2NDU3OTEwMjgy.YNyuUQ.yVKLFdkR65x-mg31Joez-ao6Erk",
+    "token": "ODU5ODUyNzk2NDU3OTEwMjgy.YNyuUQ.y2FkkSx4B8hqFjhuuR0CuVOF8Hk",
     "mongoURI": "mongodb+srv://dbUser:dbPass@bicycledb.6e694.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     "prefix": "!",
     "modmail": {
