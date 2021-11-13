fefc63b59b8152ef765f37856a05da9d7516dfc6
Tur-ph <67241029+Tur-ph@users.noreply.github.com>
2021-11-13 20:49:55 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    "token": "ODU5ODUyNzk2NDU3OTEwMjgy.YNyuUQ.y2FkkSx4B8hqFjhuuR0CuVOF8Hk",
-    "mongoURI": "mongodb+srv://dbUser:dbPass@bicycledb.6e694.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "token": "token",
+    "mongoURI": "mongoUIR",
     "prefix": "!",
     "modmail": {
         "categoryID": "categoryID",
